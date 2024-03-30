7d4f5a7ea0ce4012888c21189cf4315b8219cf7b
Krokus <orlov-bn@mail.ru>
2024-03-30 12:33:42 +0500

    first commit

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_lisr = ["pineapple", "pear","avocado", "banana", "apricot"]
+print("List: ", my_lisr)
+print("First element: ", my_lisr[0])
+print("Last element: ", my_lisr[-1])
+print("Sublist: ", my_lisr[2:4])
+my_lisr[2] = "persimmon"
+print("Modified list: ", my_lisr)
+my_dict = {"один": "one", "два": "two", "три": "three", "четыре": "four", "пять": "five"}
+print("Dictionary: ", my_dict)
+print("Translation: ", my_dict["четыре"])
+my_dict["один"] = "first"
+my_dict["шесть"] = "six"
+print("Modified dictionary: ", my_dict)
