d899a3d1ae726c0194c68b38148560b93e12a5b2
Krokus <orlov-bn@mail.ru>
2024-03-31 15:36:11 +0500

    create homework

--- homework9.py
+++ homework9.py
@@ -10,7 +10,7 @@ test(2, "helloy", 24, name="Boris", adress="Moscow")
 def factorial(n):
     if n == 1:
         return 1
-    number_n_min_1 = factorial(n=n -1)
+    number_n_min_1 = factorial(n=n - 1)
     return number_n_min_1 * n
 
 print("________________")
