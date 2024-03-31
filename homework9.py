25cdfe0f839f889f1dce732dc2946ef0b4c3728f
Krokus <orlov-bn@mail.ru>
2024-03-31 15:52:04 +0500

    Вывел на печать значение int (result)

--- homework9.py
+++ homework9.py
@@ -7,11 +7,14 @@ def test(a, *args, **kwargs):
 
 test(2, "helloy", 24, name="Boris", adress="Moscow")
 
+
 def factorial(n):
     if n == 1:
         return 1
     number_n_min_1 = factorial(n=n - 1)
     return number_n_min_1 * n
 
+
 print("________________")
-print(factorial(5))
+result = factorial(5)
+print(result)
