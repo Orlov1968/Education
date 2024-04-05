8f96e01c61014c6d7a8b725ef33e1ed9e6656abd
Krokus <orlov-bn@mail.ru>
2024-04-05 21:16:30 +0500

    Изменил main.py и создал homework10.py

--- homework10.py
+++ homework10.py
@@ -2,10 +2,9 @@ def test_1():
     print("Hello, world")
 
 
-test_1()
+
 
 def test_2(name = "Hello, world"):
     print(name)
 
 
-test_2()
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 import homework10
+
+homework10.test_1()
+homework10.test_2()
