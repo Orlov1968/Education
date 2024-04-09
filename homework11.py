1612a837ecb73a200a6a86662dabddba832537cf
Krokus <orlov-bn@mail.ru>
2024-04-10 00:03:34 +0500

    Выполнение задания по теме пространство имён

---
+++ homework11.py
@@ -0,0 +1,10 @@
+def test_function():
+    def inner_function():
+        string_ = "Я в области видимости функции test_function"
+        print(string_)
+
+    inner_function()
+
+
+#inner_function() - среда не видит эту ф-цию вне test_function()
+#чтобы распечатать string_, надо вызвать ф-цию test_function()
