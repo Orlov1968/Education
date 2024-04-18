4e1560d640e9716995ed33a691ff5889207a3a4c
Krokus <orlov-bn@mail.ru>
2024-04-18 22:58:59 +0500

    Домашнее задание по теме "Специальные методы классов"

---
+++ homework14.py
@@ -0,0 +1,10 @@
+class Buiding:
+
+    def __init__(self):
+        self.numberOfFloors = 5
+        self.buildingType = "5"
+
+    def __eq__(self, other):
+        return self.numberOfFloors == other.buildingType
+
+
