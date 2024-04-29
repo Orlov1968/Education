4e0b52b751d522b01b823ba96db32777324f00e6
Krokus <orlov-bn@mail.ru>
2024-04-29 21:51:54 +0500

    Исправил

--- homework17.py
+++ homework17.py
@@ -2,27 +2,28 @@ from colorama import Fore
 
 
 class Vehicle:
-    vehicle_type = "none"
+    def __init__(self, vehicle_type="none"):
+        self.vehicle_type = vehicle_type
 
 
 class Car:
-    price = 1000000
+    def __init__(self, price=1000000):
+        self.price = price
 
-    def horse_powers(self, horse_powers):
-        self.horse_powers = horse_powers
-        return self.horse_powers
+    def horse_powers(self):
+        return 149
 
 
 class Nissan(Vehicle, Car):
-    vehicle_type = "sedan"
-    price = 1200000
+    def __init__(self, vehicle_type, price):
+        super().__init__(vehicle_type)
+        Car.__init__(self, price)
 
-    def horse_powers(self, horse_powers):
-        self.horse_powers = horse_powers
-        return self.horse_powers
+    def horse_powers(self):
+        return 199
 
 
-nissan = Nissan()
+nissan = Nissan("sedan", 1200000)
 print(Fore.YELLOW)
 print("Тип автомобиля ", nissan.vehicle_type)
 print(Fore.MAGENTA)
