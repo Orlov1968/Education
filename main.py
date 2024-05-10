70db3b94b55af6da39c6d4017d24671b100de981
ASUS <orlov-bn@mail.ru>
2024-05-10 12:58:49 +0500

    Домашняя работа

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-file_name = "poem.txt"
-with open(file_name, "r", encoding="utf8") as file_poem:
-    content = file_poem.read()
-    print(content)
-print("Проерка")
+import os
+from datetime import time
+import time
+
+directory = "C:\\Users\\ASUS\\Downloads"
+for dirpath, dirnames, filenames in os.walk(directory):
+    for file in filenames:
+        file_path = os.path.join(dirpath, file)
+        file_time = os.path.getmtime(file_path)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(file_time))
+        file_size = os.path.getsize(file_path)
+        parent_dir = os.path.dirname(file_path)
+        print(f"Обнаружен файл: {file} , Путь: {file_path}, Размер: {file_size} байт," 
+              f"Время изменения {formatted_time}, Родительская директория {parent_dir} ")
