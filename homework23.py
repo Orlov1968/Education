942352f7531fbd77ba27593e4839142085cbb998
Krokus <orlov-bn@mail.ru>
2024-05-15 01:14:56 +0500

    Изменил код

--- homework23.py
+++ homework23.py
@@ -8,4 +8,7 @@ def add_everything_up(a, b):
             print(f"{a}{b}")
 
 
-print(add_everything_up("vvv", 9.4))
+print(add_everything_up(123.456, "строка"))
+print(add_everything_up("яблоко", 4215))
+print(f"{add_everything_up(123.456, 7):3.3f}")
+
