59aad8be3c35bc56ff21fe35de3ced918d34bfd3
Krokus <orlov-bn@mail.ru>
2024-06-18 23:38:52 +0500

    Метод serve_customer запутался совсем. Помогите

--- homework33.py
+++ homework33.py
@@ -1,29 +1,60 @@
-from queue import Queue
-from time import sleep
+import threading
+import time
+
+import queue
 
 
 class Table:
+
     def __init__(self, number):
         self.number = number
-        self.is_busy = True
+        self.is_busy = False
+
+
+class Customer(threading.Thread):
+
+    def __init__(self, number):
+        super().__init__()
+        self.number = number
+
+    def run(self):
+        cafe.serve_customer()
 
 
 class Cafe:
+
     def __init__(self, tables):
         self.tables = tables
-        self.queue = Queue(3)
+        self.queue = queue.Queue()
 
     def customer_arrival(self):
-        for i in range(1, 20):
-            print(f"Посетитель номер{i} прибыл")
-            sleep(1)
-            self.queue.put(i)
+        customer_number = 1
+        while customer_number <= 20:
+            print(f"Посетитель номер {customer_number} прибыл")
+            customer = Customer(customer_number)
+            customer.start()
+            time.sleep(1)
+            customer_number += 1
 
     def serve_customer(self, customer):
-        while True:
-            item = self.queue.get()
-            if item is None:
-                break
-        print(f"Посетитель{item} сел за стол {self.tables}")
-        print(f"Посетитель{item} покушал и ушёл")
-        sleep(5)
+        table_found = True
+        for t in tables:
+            if t == table_found:
+                print(f"Посетитель {customer} сел за стол {tables[t]}.")
+                time.sleep(5)
+                print(f"Посетитель номер {customer} покушал и ушёл.")
+            else:
+                print(f"Посетитель {customer} ожидает свободный стол")
+
+
+table_1 = Table(1)
+table_2 = Table(2)
+table_3 = Table(3)
+tables = [table_1, table_2, table_3]
+
+cafe = Cafe(tables)
+
+customer_arrival_thread = threading.Thread(target=cafe.customer_arrival())
+customer_arrival_thread.start()
+
+customer_arrival_thread.join()
